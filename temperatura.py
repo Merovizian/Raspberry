f5cb63ee76426cd4e5e08850514ece37441196c0
Merovizian <ericgmicaela@gmail.com>
2021-10-19 00:38:00 -0300

    V1

---
+++ temperatura.py
@@ -0,0 +1,55 @@
+from time import time, sleep
+from datetime import date,datetime
+from gpiozero import CPUTemperature
+
+cpu = CPUTemperature()
+temp = (cpu.temperature)
+
+data_atual = str(date.today())
+time_atual = time()
+texto = list()
+
+horario = datetime.now().strftime("%H:%M:%S")
+
+
+
+def arquivoExiste(nome):
+    '''
+    Verifica se um arquivo já existe, se não o cria.
+    :param nome: arquivo
+    :return:1 - Se o arquivo já existe e 0 - se não existe
+    '''
+    try:
+        a = open(nome,'rt')
+        a.close()
+    except FileNotFoundError:
+        return 0
+    else:
+        return 1
+
+arquivo = data_atual
+if arquivoExiste(arquivo) == 0:
+    a = open(arquivo, 'wt+')
+    print(f"Arquivo {arquivo} criado com sucesso!")
+else:
+    a = open(arquivo, 'a')
+    print(f"Arquivo {arquivo} já existente")
+
+
+a.writelines(f"{data_atual}\n")
+a.writelines(f"   Time  - Temperatura\n")
+
+while True:
+    try:
+        if(time() - time_atual >= 10 ):
+            a.writelines(f" {horario}    {temp}\n")
+            time_atual = time()
+           
+    except:
+        a.writelines(f"\n\n")
+        a.close()
+        print(f"Arquivo {arquivo} gravado com sucesso!")
+        break;
+
+
+    
